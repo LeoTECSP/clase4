0030355963488e70ae2bdeea7a8cb81edb497bc1
Leonardo L <lloza.milan@gmail.com>
2025-04-24 18:44:36 -0600

    Agregar archivo de Saludo

---
+++ holamundo.cs
@@ -0,0 +1,8 @@
+//Este programa imprime "Hola Mundo" en la consola
+
+using System;
+
+Console.WriteLine("Hola Mundo"); // Imprime "Hola Mundo" en la consola
+Console.ReadLine(); // Espera a que el usuario presione una tecla antes de cerrar la consola
+
+
